eb5aa3b29d55781b82ef497b7c5fa57840ceccf4
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2023-02-23 00:45:05 +0300

    feat(FF-1788): autoprefixer postcss plugin

--- scripts/compile/post-process-css.ts
+++ scripts/compile/post-process-css.ts
@@ -1,6 +1,7 @@
+import autoprefixer from 'autoprefixer';
 import postcss from 'postcss';
 
 export async function postProcessCss({ css, from }: { css: string; from: string }) {
-  const postCssPlugins = [require('postcss-discard-comments')];
+  const postCssPlugins = [autoprefixer(), require('postcss-discard-comments')];
   return postcss(postCssPlugins).process(css, { from });
 }
