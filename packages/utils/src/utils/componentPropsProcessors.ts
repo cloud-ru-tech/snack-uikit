05fe56d43ba10713e3af24c4d6b51c38846625a9
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2023-03-04 16:09:30 +0300

    feat(FF-1870): component props processors has been added

---
+++ packages/utils/src/utils/componentPropsProcessors.ts
@@ -0,0 +1,46 @@
+import { AriaAttributes } from 'react';
+
+const DATA_REGEXP = /^data-/;
+const DATA_TEST_REGEXP = /^data-test-/;
+const DATA_TEST_AND_ARIA_REGEXP = /^(data-test|aria)-/;
+
+function excludeProps<T extends Record<string, unknown>>(props: T, regexp: RegExp) {
+  return Object.keys(props)
+    .filter(prop => !prop.match(regexp))
+    .reduce(
+      (nextProps, prop) => ({
+        ...nextProps,
+        [prop]: props[prop],
+      }),
+      {},
+    );
+}
+
+function extractProps<T extends Record<string, unknown>>(props: T, regex: RegExp) {
+  return Object.keys(props).reduce((nextProps, prop) => {
+    if (prop.match(regex)) nextProps[prop] = props[prop];
+
+    return nextProps;
+  }, {} as Record<string, unknown>);
+}
+
+export type WithSupportProps<T> = {
+  'data-test-id'?: string;
+} & AriaAttributes &
+  T;
+
+export function excludeSupportProps<T extends Record<string, unknown>>(props: T) {
+  return excludeProps(props, DATA_TEST_AND_ARIA_REGEXP);
+}
+
+export function extractDataProps<T extends Record<string, unknown>>(props: T) {
+  return extractProps(props, DATA_REGEXP);
+}
+
+export function extractDataTestProps<T extends Record<string, unknown>>(props: T) {
+  return extractProps(props, DATA_TEST_REGEXP);
+}
+
+export function extractSupportProps<T extends Record<string, unknown>>(props: T) {
+  return extractProps(props, DATA_TEST_AND_ARIA_REGEXP);
+}
