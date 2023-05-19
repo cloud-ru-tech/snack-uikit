ab22626c951b601b1bd01841bdbc328ea8a3310d
Anna Kozlova <apkozlova@sbercloud.ru>
2023-05-19 17:55:12 +0300

    chore(FF-2376): check direct prod deps by license-checker

--- scripts/checkLicenses.ts
+++ scripts/checkLicenses.ts
@@ -1,22 +1,38 @@
+import path from 'path';
+
 import checker, { InitOpts, ModuleInfos } from 'license-checker';
 
-const ALLOWED_LICENSES = [
-  'Unlicense',
-  'MIT',
-  'ISC',
-  'Apache-2.0',
-  'WTFPL',
-  'BSD',
-  'MPL-2.0',
-  'CC0-1.0',
-  'CC-BY-3.0',
-  'CC-BY-4.0',
-  'Public Domain',
-  'WTFPL',
-  'BlueOak-1.0.0',
-  'Zlib',
-].join(', ');
-const EXCLUDED_PACKAGES_SCOPES = ['@sbercloud', '@snack-ui'];
+import packageJson from '../package.json';
+import { logError, logInfo } from './utils/console';
+import { getAllPackageFolders } from './utils/getAllPackageFolders';
+
+type PackageJson = {
+  dependencies?: Record<string, string>;
+  devDependencies?: Record<string, string>;
+  peerDependencies?: Record<string, string>;
+};
+
+const ALLOWED_LICENSES = ['MIT', 'ISC', 'Apache-2.0', 'BSD', 'Public Domain'].join(', ');
+const EXCLUDED_PACKAGE_SCOPES = ['@snack-ui'];
+
+function getAllDeps(packageJson?: PackageJson) {
+  if (!packageJson) {
+    return [];
+  }
+
+  const allDeps = {
+    ...packageJson.dependencies,
+    ...packageJson.peerDependencies,
+  };
+
+  return Object.entries(allDeps)
+    .filter(([packageName]) => !EXCLUDED_PACKAGE_SCOPES.some(scope => packageName.startsWith(scope)))
+    .map(([packageName, version]) => `${packageName}@${version}`);
+}
+
+function getPackageJson(folder: string): PackageJson {
+  return require(path.resolve(folder, 'package.json'));
+}
 
 function getLicenseInfo(props: InitOpts): Promise<ModuleInfos> {
   return new Promise((resolve, reject) => {
@@ -40,22 +56,29 @@ function getLicenseInfo(props: InitOpts): Promise<ModuleInfos> {
 
 (async () => {
   try {
-    const commonLicenseInfo = Object.entries(await getLicenseInfo({ start: './' })).filter(
-      ([packageName]) => !EXCLUDED_PACKAGES_SCOPES.some(scope => packageName.startsWith(scope)),
-    );
+    const folders = getAllPackageFolders();
 
+    const depsToCheck = [
+      ...new Set([
+        ...getAllDeps(packageJson),
+        ...folders.flatMap(folder => getAllDeps(getPackageJson(folder))),
+      ]).values(),
+    ];
+
+    console.log('The followings deps are checked:');
+    console.log(depsToCheck.map(dep => ` * ${dep}`).join('\n'));
+
+    const commonLicenseInfo = Object.entries(await getLicenseInfo({ start: './', packages: depsToCheck.join(';') }));
     if (commonLicenseInfo.length <= 0) {
-      console.log('All licenses are allowed');
+      logInfo('\nAll licenses are allowed');
       return;
     }
 
-    console.log('The following packages has incorrect licenses:');
+    logError('[ERROR] The following packages have not valid licenses:');
 
-    commonLicenseInfo
-      .map(([packageName, { licenses }]) => ` * ${packageName}: ${licenses}`)
-      .forEach(item => console.log(item));
+    logError(commonLicenseInfo.map(([packageName, { licenses }]) => ` * ${packageName}: ${licenses}`).join('\n'));
 
-    console.log(`\nThe list of allowed licenses: ${ALLOWED_LICENSES}`);
+    logError(`The list of allowed licenses: ${ALLOWED_LICENSES}`);
     process.exit(1);
   } catch (err) {
     console.log(err);
