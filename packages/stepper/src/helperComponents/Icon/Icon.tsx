c1163ff4087a0cd518d7ef2e07b9fbd791f9e9cf
Nikita Ershov <niershov@cloud.ru>
2023-10-27 11:27:31 +0300

    style(FF-3460): update colors; apply renamed tokens

--- packages/stepper/src/helperComponents/Icon/Icon.tsx
+++ packages/stepper/src/helperComponents/Icon/Icon.tsx
@@ -1,7 +1,9 @@
 import cn from 'classnames';
+import { useMemo } from 'react';
 
 import { CheckSVG, CrossSVG } from '@snack-ui/icons';
 import { Sun } from '@snack-ui/loaders';
+import { Typography } from '@snack-ui/typography';
 
 import { StepState } from '../../constants';
 import styles from './styles.module.scss';
@@ -26,9 +28,11 @@ function getContent(state: StepState, number: number) {
 }
 
 export function Icon({ state, number, className }: IconProps) {
+  const content = useMemo(() => getContent(state, number), [number, state]);
+
   return (
     <div data-state={state} className={cn(styles.icon, className)}>
-      {getContent(state, number)}
+      {typeof content === 'number' ? <Typography.SansLabelL>{content}</Typography.SansLabelL> : content}
     </div>
   );
 }
