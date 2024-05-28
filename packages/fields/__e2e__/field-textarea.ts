47c04b997f951066dd3c2d8bd691c9918e0ae53e
Nikita Ershov <niershov@cloud.ru>
2024-05-28 09:31:04 +0300

    chore(FF-0000): temp skip chrome test

--- packages/fields/__e2e__/field-textarea.ts
+++ packages/fields/__e2e__/field-textarea.ts
@@ -7,7 +7,6 @@ fixture('Field TextArea').skipJsErrors(args => Boolean(args?.message?.includes('
 
 const TEST_ID = 'field-textarea-test';
 const COMPONENT_PREFIX = 'field-textarea';
-const RESIZER = '.os-scrollbar-corner-resize';
 
 const getScrollArea = (wrapper: Selector) => wrapper.find(dataTestIdSelector('field-textarea__scroll-area'));
 
@@ -47,34 +46,4 @@ test.page(visit({ value: '', maxRows: 4 }))("Shouldn't get bigger than maxRows p
 
   const height2 = await getScrollArea(wrapper).offsetHeight;
   await t.expect(input.offsetHeight).gt(height2);
-});
-
-// TODO: TestCafe cannot interact with the <div data-overlayscrollbars-contents=""
-// resizable
-// eslint-disable-next-line testcafe-community/no-disabled-tests
-test.skip.page(visit({ value: '', resizable: true }))('Should be resizable', async t => {
-  const wrapper = Selector(dataTestIdSelector(TEST_ID));
-  const input = getInputInner(wrapper);
-
-  await t.typeText(input, '1\n2\n3\n4\n5\n6');
-
-  const height1 = await getScrollArea(wrapper).offsetHeight;
-  await t.expect(input.offsetHeight).gt(height1);
-
-  await t.drag(Selector(RESIZER), 0, 100);
-
-  const height2 = await getScrollArea(wrapper).offsetHeight;
-  await t.expect(input.offsetHeight).lt(height2);
-});
-
-// readonly & resizable
-// eslint-disable-next-line testcafe-community/no-disabled-tests
-test.skip.page(visit({ resizable: true, readonly: true }))('Should not be resizable when readonly = true', async t => {
-  await t.expect(Selector(RESIZER).exists).notOk("resizer is present although shouldn't");
-});
-
-// disabled & resizable
-// eslint-disable-next-line testcafe-community/no-disabled-tests
-test.skip.page(visit({ resizable: true, disabled: true }))('Should not be resizable when disabled = true', async t => {
-  await t.expect(Selector(RESIZER).exists).notOk("resizer is present although shouldn't");
 });
