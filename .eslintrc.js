4413c88326e246add7b1bdd7822ae20fe1abbb82
Anna Kozlova <apkozlova@cloud.ru>
2024-09-23 12:02:07 +0300

    chore(PDS-480): add ssr eslint preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['@cloud-ru/eslint-config/monorepo', 'plugin:testcafe-community/recommended'],
+  extends: [
+    '@cloud-ru/eslint-config/monorepo',
+    'plugin:testcafe-community/recommended',
+    'plugin:@cloud-ru/ssr-safe-react/recommended',
+  ],
   plugins: ['testcafe-community'],
   rules: {
     '@typescript-eslint/no-namespace': 'off',
@@ -11,18 +15,21 @@ module.exports = {
       files: ['packages/**/__e2e__/**/*.ts'],
       rules: {
         'testcafe-community/missing-expect': 'off',
+        '@cloud-ru/ssr-safe-react/domApi': 'off',
       },
     },
     {
       files: ['packages/**/__tests__/**/*.ts'],
       rules: {
         'vitest/consistent-test-it': ['error', { fn: 'it' }],
+        '@cloud-ru/ssr-safe-react/domApi': 'off',
       },
     },
     {
       files: ['packages/*/stories/**/*', 'storybook/stories/**/*'],
       rules: {
         'react/function-component-definition': 'off',
+        '@cloud-ru/ssr-safe-react/domApi': 'off',
       },
     },
     {
