7982dbf1c203ab09c148f91eb96e148710fe9b05
Nikita Ershov <niershov@cloud.ru>
2024-11-28 18:55:24 +0300

    feat(PDS-801): use typography prop

--- packages/notification/src/components/NotificationCard/components/NotificationCardSkeleton.tsx
+++ packages/notification/src/components/NotificationCard/components/NotificationCardSkeleton.tsx
@@ -1,29 +1,27 @@
-import { Skeleton, SkeletonText } from '@snack-uikit/skeleton';
+import { SkeletonText } from '@snack-uikit/skeleton';
 
 import { TEST_IDS } from '../../NotificationPanel/constants';
 import styles from '../styles.module.scss';
 
-const BORDER_RADIUS = 24;
-
 export function NotificationCardSkeleton() {
   return (
     <div className={styles.notificationCard} data-test-id={TEST_IDS.skeleton} data-skeleton={true}>
-      <Skeleton width={140} height={11} borderRadius={BORDER_RADIUS} />
+      <SkeletonText width={140} typography='label-s' lines={1} />
 
       <div className={styles.notificationCardTitle} data-center={true}>
         <div className={styles.notificationCardTitleIcon}>
-          <Skeleton width={16} height={16} borderRadius={BORDER_RADIUS} />
+          <SkeletonText width={16} typography='title-s' lines={1} />
         </div>
-        <Skeleton width={140} height={14} borderRadius={BORDER_RADIUS} />
+        <SkeletonText width={140} typography='title-s' lines={1} />
       </div>
 
       <div className={styles.notificationCardContent}>
-        <SkeletonText lines={3} borderRadius={BORDER_RADIUS} />
+        <SkeletonText lines={3} typography='body-s' />
       </div>
 
       <div className={styles.notificationCardFooter}>
-        <Skeleton width={140} borderRadius={BORDER_RADIUS} height={12} />
-        <Skeleton width={140} borderRadius={BORDER_RADIUS} height={11} />
+        <SkeletonText width={140} typography='body-s' lines={1} />
+        <SkeletonText width={140} typography='label-s' lines={1} />
       </div>
     </div>
   );
