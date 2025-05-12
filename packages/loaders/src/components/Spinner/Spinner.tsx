8c3dfb2d0d12567903ed114083cabc7db8a7bc74
Sergei Khlupin <svhlupin@cloud.ru>
2025-05-12 17:51:29 +0300

    fix(FF-6240): loader rotation center

--- packages/loaders/src/components/Spinner/Spinner.tsx
+++ packages/loaders/src/components/Spinner/Spinner.tsx
@@ -25,13 +25,8 @@ export function Spinner({ size = LOADER_SIZE.S, className, ...rest }: SpinnerPro
         {...extractSupportProps(rest)}
         data-size={size}
       >
-        <mask id='spinnerMask'>
-          <circle opacity='0.24' cx='4' cy='4' r='3' strokeWidth='1.5' />
-          <path d='M1 4C1 2.34315 2.34315 1 4 1' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
-        </mask>
-        <g mask='url(#spinnerMask)'>
-          <path d='M0 0H8V8H0V0Z' />
-        </g>
+        <circle opacity='0.24' cx='4' cy='4' r='3' strokeWidth='1.5' />
+        <path d='M1 4C1 2.34315 2.34315 1 4 1' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
       </svg>
     );
   }
@@ -48,21 +43,12 @@ export function Spinner({ size = LOADER_SIZE.S, className, ...rest }: SpinnerPro
         {...extractSupportProps(rest)}
         data-size={size}
       >
-        <mask id='spinnerMask'>
-          <circle opacity='0.24' cx='8' cy='8.7951' r='6' strokeWidth='1.5' />
-          <path
-            d='M2 8.79511C2 5.4814 4.68629 2.7951 8 2.7951'
-            strokeWidth='1.5'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          />
-        </mask>
-        <g mask='url(#spinnerMask)'>
-          <path d='M0 0.795105H16V16.7951H0V0.795105Z' />
-        </g>
+        <circle opacity='0.24' cx='8' cy='8' r='6' strokeWidth='1.5' />
+        <path d='M2 8C2 4.68629 4.68629 2 8 2' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
       </svg>
     );
   }
+
   return (
     <svg
       viewBox='0 0 24 24'
@@ -72,18 +58,8 @@ export function Spinner({ size = LOADER_SIZE.S, className, ...rest }: SpinnerPro
       {...extractSupportProps(rest)}
       data-size={size}
     >
-      <mask id='spinnerMask'>
-        <circle opacity='0.24' cx='11.8926' cy='12' r='9' strokeWidth='1.5' />
-        <path
-          d='M2.89258 12C2.89258 7.02944 6.92202 3 11.8926 3'
-          strokeWidth='1.5'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-      </mask>
-      <g mask='url(#spinnerMask)'>
-        <path d='M0 0H24V24H0V0Z' />
-      </g>
+      <circle opacity='0.24' cx='12' cy='12' r='9' strokeWidth='1.5' />
+      <path d='M3 12C3 7.02944 7.02944 3 12 3' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
     </svg>
   );
 }
