a10e5baca354718d84ed836ed41e8bfe1edf4815
Данил Панов <dapanov@cloud.ru>
2025-06-19 13:49:07 +0300

    fix(ONB-3885): tooltip offset prop

--- packages/tooltip/src/components/Tooltip/Tooltip.tsx
+++ packages/tooltip/src/components/Tooltip/Tooltip.tsx
@@ -16,6 +16,7 @@ export type TooltipProps = WithSupportProps<
     PopoverPrivateProps,
     | 'className'
     | 'triggerClassName'
+    | 'offset'
     | 'open'
     | 'onOpenChange'
     | 'hoverDelayOpen'
