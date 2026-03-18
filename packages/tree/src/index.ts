20548ec3cdde2380bd67cb215b69aaf80513f11c
Vitalii Shalunov <vsshalunov@cloud.ru>
2026-03-18 11:15:21 +0300

    feat(IAM-6342): reexport tree utils

--- packages/tree/src/index.ts
+++ packages/tree/src/index.ts
@@ -1,3 +1,4 @@
 export * from './components';
-export type { OnNodeClick, TreeNodeId, TreeNodeProps } from './types';
+export type { OnNodeClick, TreeNodeId, TreeNodeProps, ExtendedTreeNodeProps } from './types';
 export { setNonce } from '@snack-uikit/list';
+export * from './helpers';
